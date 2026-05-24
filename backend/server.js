9b427abe5c8b2b209f195f37b18eb4839db04f9a
Your Name <your@email.com>
2026-05-24 19:42:56 +0530

    Update CORS origins

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,8 @@ app.use(cors({
   origin: [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
-    "https://elegant-scone-4fdca5.netlify.app"
+    "http://0.0.0.0:8080",
+    "http://10.107.187.85:8080",
   ],
   credentials: true
 }));
